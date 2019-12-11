42511e293e2686e8aaac80e7f3dc4395d8501430
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2019-12-11 14:53:57 -0800

    add basic components for file conversion

---
+++ separate-channels.sh
@@ -0,0 +1,3 @@
+convert mona-lisa-leonardo-da-vinci-la-gioconda-oil-painting-40997.jpeg -channel R  -separate monared.jpg
+convert mona-lisa-leonardo-da-vinci-la-gioconda-oil-painting-40997.jpeg -channel G  -separate monagreen.jpg
+convert mona-lisa-leonardo-da-vinci-la-gioconda-oil-painting-40997.jpeg -channel B  -separate monablue.jpg
