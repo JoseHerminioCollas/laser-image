a07c8820a0832df765c6e6e2cfc82a4a91868711
Jose Collas <joseherminiocollas@goatstonc.com>
2019-12-23 17:38:16 -0800

    add lookup for colors

--- run-jimp.js
+++ run-jimp.js
@@ -4,53 +4,63 @@ const fA = 'mona-small.png'
 const fB = 'test-image.png'
 const fC = 'square20x20.png'
 const fD = 'color-chart.jpg'
-Jimp.read(fD)
+
+Jimp.read(fA)
   .then(lenna => {
-    console.log(lenna.bitmap)
     const out = []
     for (const { x, y, idx, img } of lenna.scanIterator(
-      0,
-      0,
-      100,
-      100
+      0, 0, 100, 100
     )) {
-      var r = lenna.bitmap.data[idx + 0];
-      var g = lenna.bitmap.data[idx + 1];
-      var b = lenna.bitmap.data[idx + 2];
-      const total = r + g + b
       const colors = [
         {
           name: 'red',
-          val: r,
-          symbol: 'R'
+          val: lenna.bitmap.data[idx + 0],
+          symbol: 'r'
         }, {
           name: 'green',
-          val: g,
-          symbol: 'G'
+          val: lenna.bitmap.data[idx + 1],
+          symbol: 'g'
 
         }, {
           name: 'blue',
-          val: b,
-          symbol: 'B'
+          val: lenna.bitmap.data[idx + 2],
+          symbol: 'b'
+        },
+        {
+          name: 'white',
+          symbol: 'X'
+        },
+        {
+          name: 'black',
+          symbol: ' '
+        }, {
+          name: 'unknown',
+          symbol: '?'
         }
       ]
-      // max pixel N below total
-      const maxPixel = colors.reduce((acc, el, i) => {
-        const maxColor = el.val > acc.val ? el : acc
-        // console.log(maxColor, acc.val, el.val)
-        return maxColor
-      }, colors[1])
-      // console.log(maxPixel)
-      if (total < 100) { out.push('.') }
-      else if (total > 800) { out.push('X') }
-      else out.push(maxPixel.symbol)
-      // else if (g > 200) out.push('G')
-      // else if (b > 200) out.push('B')
-
-      // else {out.push('?')}
-      if (x === 99) out.push('|\n |')
+      let selectedSymbol = colors.filter(c => c.name === 'unknown')[0]
+      const maxColor = colors
+        .reduce((acc, el, i) => el.val > acc.val ? el : acc, colors[0])
+      const totalColorsVal = colors
+        .filter(e => ['red', 'green', 'blue'].includes(e.name))
+        .reduce((acc, color) => acc += color.val, 0)
+      // find and set the selected color
+      if (totalColorsVal < 100)
+        selectedSymbol = colors.filter(c => c.name === 'black')[0]
+      else if (totalColorsVal > 280) {
+        selectedSymbol = colors.filter(c => c.name === 'white')[0]
+      }
+      else {
+        selectedSymbol = maxColor
+      }
+      // generate the out display
+      if (x === 0) out.push('|')
+      out.push(selectedSymbol.symbol)
+      if (x === 99) out.push('|\n')
     }
+    console.log('-'.repeat(lenna.getWidth() + 2))
     console.log(out.join(''))
+    console.log('-'.repeat(lenna.getWidth() + 2))
   })
   .catch(err => {
     console.error(err)
