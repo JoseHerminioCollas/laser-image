988451c8b716a5e9469fcb27c992a4ff46f214c0
Jose Collas <joseherminiocollas@goatstonc.com>
2019-12-24 19:00:00 -0800

    refactor

--- run-jimp.js
+++ run-jimp.js
@@ -1,26 +1,29 @@
 var Jimp = require('jimp');
 const SVGServer = require('./svg-server')
 const colors = require('./colors')
-const fileName = 'mona-lisa-leonardo-da-vinci-la-gioconda-oil-painting-40997.jpeg'
-const fA = 'mona-small.png'
-const fB = 'test-image.png'
-const fC = 'square20x20.png'
-const fD = 'color-chart.jpg'
+const images = [
+  'mona-small.png',
+  'mona-lisa-leonardo-da-vinci-la-gioconda-oil-painting-40997.jpeg',
+  'test-image.png',
+  'square20x20.png',
+  'color-chart.jpg'
+]
 
-
-Jimp.read(fA)
+Jimp.read(images[0])
   .then(lenna => {
-    const out = [...Array(149)].map(() => [...Array(100)])
+    const imageColors = [...Array(149)].map(() => [...Array(100)]) // TODO get width and height of image
     for (const { x, y, idx, img } of lenna.scanIterator(
       0, 0, 100, 149
     )) {
       colors.red.val = lenna.bitmap.data[idx + 0]
       colors.green.val = lenna.bitmap.data[idx + 1]
       colors.blue.val = lenna.bitmap.data[idx + 2]
-      let selectedColor = colors.unknown.fillValue
+      let selectedColor = colors.unknown
       const maxColor = Object.entries(colors)
-        .reduce((acc, el, i) => el[1].val > acc.val ? el[1] : acc, colors.red)
-      const totalColorsVal = colors.red.val + colors.green.val + colors.blue.val
+        .reduce((acc, color) => color[1].val > acc.val ? color[1] : acc, colors.red)
+      const totalColorsVal = Object.entries(colors)
+        .filter(color => ['red', 'green', 'blue'].includes(color[1].name))
+        .reduce((acc, color) => acc + color[1].val, 0)
 
       if (totalColorsVal < 150) // black
         selectedColor = colors.black
@@ -31,25 +34,28 @@ Jimp.read(fA)
       }
 
       // set the result that will be used
-      out[y][x] = selectedColor
+      imageColors[y][x] = selectedColor
     }
-    const contentString = out.map((element, colI) => {
-      let str = ''
-      str += element.map((e, rowI) => {
-        return `<circle
-          r="0.5"
-          cx="${rowI}" 
-          cy="${colI}"
-          fill="${e.fillValue}"
-          stroke="none"
-          />`
-      }).join('')
-      return str
-    })
-      .join('')
-
-    SVGServer(contentString)
+    SVGServer(colorsToSVG(imageColors))
   })
   .catch(err => {
     console.error(err)
   })
+function colorsToSVG(colors) {
+  const contentString = colors.map((element, colI) => {
+    let str = ''
+    str += element.map((e, rowI) => {
+      return `<circle
+        r="0.5"
+        cx="${rowI}" 
+        cy="${colI}"
+        fill="${e.fillValue}"
+        stroke="none"
+        />`
+    }).join('')
+    return str
+  })
+    .join('')
+
+  return contentString
+}
