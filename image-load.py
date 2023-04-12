41356e9d4fe1ef60b34032311c752e350b66c296
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-04-12 17:34:31 -0400

    WIP

--- image-load.py
+++ image-load.py
@@ -1,16 +1,10 @@
 import cv2
 # Load an color image in grayscale
-#img = cv2.imread('mona-small.png',0)
+img = cv2.imread('mona-small.png',0)
 
 #Display the image
-#cv2.imshow('image',img)
+cv2.imshow('image',img)
 #key binding function
-#cv2.waitKey(0)
+cv2.waitKey(0)
 #Destroyed all window we created earlier.
-#cv2.destroyAllWindows()
-# create a file
-f = open('xa.txt', 'w')
-f.write('aXXXxxxx')
-#b = f.read()
-print('aaa')
-#print(b)
+cv2.destroyAllWindows()
