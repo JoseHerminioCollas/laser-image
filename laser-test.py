56939e5a5f29b600f28f4b2f87175035d6c13198
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-04-12 11:37:39 -0400

    WIP

---
+++ laser-test.py
@@ -0,0 +1,9 @@
+# laser-test.py
+# create an SVG file that can be used for laser cutting
+
+fileName = 'test-a.svg'
+f = open(fileName, 'w')
+# write
+f.write('XXX')
+b = open(fileName, 'r')
+print(b)
