33e747aaff53969a3b98aa06593a90538c9c449e
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-04-12 12:12:42 -0400

    WIP

--- laser-test.py
+++ laser-test.py
@@ -2,8 +2,15 @@
 # create an SVG file that can be used for laser cutting
 
 fileName = 'test-a.svg'
+r = 10
+tag = '<circle r={0} cx="100" cy="100" fill="none" stroke="0.001" />'.format(r)
 f = open(fileName, 'w')
 # write
-f.write('XXX')
-b = open(fileName, 'r')
-print(b)
+f.write('<svg>')
+f.write(tag)
+f.write('<svg>')
+
+# ??? I do not see this printing to the screen 
+# b = open(fileName, 'r')
+# c = b.readline()
+# xsprint('xx{0}'.format(c))
