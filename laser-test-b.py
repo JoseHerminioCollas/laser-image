a9e4f5fb636983e83f071f5e56dae070e0986c20
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-04-12 22:31:22 -0400

    WIP

--- laser-test-b.py
+++ laser-test-b.py
@@ -1,61 +1,52 @@
+import cv2
+# Load an color image in grayscale
+img = cv2.imread('mona-small.png',0)
+r,c = img.shape
+rowS = ''
+for i in range(r):
+ for j in range(c):
+  k = img[i,j]
+  l = 255 - k
+  #if k>220: 
+   #print(k)
+  #rowS += str(k)
+  #if(k > 85):
+  cx = 4 * j
+  cy = 4 * i
+  radius = k * 0.008
+  rowS += '<circle r="{2}" cx="{0}" cy="{1}" />'.format(cx, cy, radius)
+  #else:
+  #   rowS += 'XXX'
+ #print(rowS)
+  
+#Display the image
+ # cv2.imshow('image',img)
+#key binding function
+ # cv2.waitKey(0)
+#Destroyed all window we created earlier.
+ # cv2.destroyAllWindows()
+
 # laser-test.py
 # create an SVG file that can be used for laser cutting
 
 # create the file
-fileName = 'laser-test.svg'
+fileName = 'laser-test-b.svg'
 f = open(fileName, 'w')
 
-def circleTags(radius, colWidth, rowWidth):
- tag = ''
- rowCount = 0
- totalRowCount = 10
- while rowCount < totalRowCount:
-  rowIncrement = rowCount * rowWidth
-  colCount = 0
-  totalColCount = 10
-  while colCount < totalColCount:
-   colIncrement = colCount * colWidth
-   tag += '<circle r="{0}" cx="{1}" cy="{2}" fill="none" stroke="black" stroke-width="0.1" />'.format(radius, colIncrement, rowIncrement)
-   colCount += 1
-  rowCount += 1
- return tag
+# create a file, write to a file, write SVG tags
+# create a file
+# f = open('laser-test-b.svg', 'w')
+# write
+# f.write('XXX')
+# b = open('test.txt', 'r')
+# print('aaa')
+# print(b)
 
-colWidth = 2
-rowWidth = 2
-radius = '0.1'
-c = circleTags(radius, colWidth, rowWidth)
 # write the contents of the SVG file
-f.write('<svg width="200" height="200">')
-
-f.write('<g transform="translate(10,10)">')
-f.write(c)
-f.write('</g>')
-
-f.write('<g transform="translate(10,35)">')
-f.write(circleTags('0.5', 2, 2))
-f.write('</g>')
-
-f.write('<g transform="translate(10,70)">')
-f.write(circleTags('1', 3, 3))
-f.write('</g>')
-
-f.write('<g transform="translate(10,110)">')
-f.write(circleTags('1', 6, 6))
-f.write('</g>')
-
-f.write('<g transform="translate(40,10)">')
-f.write(circleTags('2', 6, 6))
-f.write('</g>')
-
-f.write('<g transform="translate(90,90)">')
-f.write(circleTags('3', 9, 9))
-f.write('</g>')
-
-f.write('<text x="100" y="10" font-size="0.5mm">VERICITE</text>')
-f.write('<text x="100" y="15" font-size="1mm">VERICITE</text>')
-f.write('<text x="100" y="25" font-size="2mm">VERICITE</text>')
-f.write('<text x="100" y="40" font-size="3mm">VERICITE</text>')
-f.write('<text x="100" y="55" font-size="4mm">VERICITE</text>')
-f.write('<text x="100" y="75" font-size="5mm">VERICITE</text>')
-
-f.write('<svg>')
+f.write('<svg width="400" height="600">')
+f.write(rowS)
+# f.write('<g transform="translate(10,10)">')
+# f.write(c)
+# f.write('</g>')
+# f.write('<text x="100" y="25" font-size="2mm">VERICITE</text>')
+f.write('</svg>')
