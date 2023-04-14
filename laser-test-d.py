0c9af618032cdea6e9814b105f0c62c2f14b0f72
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2023-04-14 10:04:50 -0400

    WIP

--- laser-test-d.py
+++ laser-test-d.py
@@ -3,20 +3,23 @@
 import math
 
 rowS = ''
-offSet = 70
-r = 50
+offSet = 20
+r = 1
 # for 12 steps 1/12 1/steps
-steps = 12
-angleIncrement = 0.17
+steps = 122
+angleIncrement = 0.1
+tagString = '<circle r="{0}" cx="{2}" cy="{1}" fill="none" stroke="black" stroke-width="0.01mm" />'
 
 i = 0
-while i<steps:
- print(i)
+while i < steps:
  angleX = angleIncrement * 3.14 * i
  angleY = angleIncrement * 3.14 * i
- x = math.cos(angleX) * r + offSet
- y = math.sin(angleY) * r + offSet
- rowS += '<circle r="{0}" cx="{2}" cy="{1}" fill="none" stroke="black" stroke-width="0.01mm" />'.format(i + 1, y, x )
+ spiralRadius = r + (i * 0.1)
+ #spiralRadius = r
+ x = math.cos(angleX) * spiralRadius + offSet
+ y = math.sin(angleY) * spiralRadius + offSet
+ circleRadius = 0.1 # (i * 0.05) + 1
+ rowS += tagString.format(circleRadius, y, x)
  i += 1
   
 # create the file
@@ -24,7 +27,7 @@ fileName = 'laser-test-d.svg'
 f = open(fileName, 'w')
 
 # write the contents of the SVG file
-f.write('<svg width="150mm" height="240mm">')
+f.write('<svg width="40" height="40">')
 f.write(rowS)
-f.write('<text x="100" y="239" font-size="1mm">VERICITE</text>')
+f.write('<text x="10" y="40" font-size="1mm">VERICITE</text>')
 f.write('</svg>')
