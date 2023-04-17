a269cc037ef0730050001f7f6e60855c0994cffd
joseherminiocollas <joseherminiocollas@goatstone.com>
2023-04-16 22:18:38 -0400

    WIP

--- laser-test-h.py
+++ laser-test-h.py
@@ -2,12 +2,11 @@ import math
 
 rowS = ''
 offSet = 30
-#r = 0 # math.pi * 0.6 # spiral radius starr
-steps = 100
+steps = 190
 currAngle = 0
-angleIncrement = math.pi * 0.25 # * 0.06
+angleIncrement = math.pi * 0.4 # * 0.06
 spiralRadius = 3
-spiralRadiusIncrement = 0.75
+spiralRadiusIncrement = 0.7
 tagString = '<circle r="{0}" cx="{2}" cy="{1}" fill="none" stroke="black" stroke-width="0.01mm" />'
 
 i = 0
@@ -15,32 +14,19 @@ while i <= steps:
  angleX = currAngle
  angleY = currAngle
  currAngle = currAngle + angleIncrement
- #angleIncrement = angleIncrement * 0.95
- #if angleIncrement < 0.6:
-  #angleIncrement = angleIncrement * 0.95
- #if angleIncrement <= 0.15:
-  #angleIncrement = 0.15
-
+ angleIncrement = angleIncrement * 0.9
+ if angleIncrement <= 0.15:
+  angleIncrement = 0.15
  x = math.cos(angleX) * spiralRadius + offSet
  y = math.sin(angleY) * spiralRadius + offSet
  spiralRadius = spiralRadius + spiralRadiusIncrement
- #if i < 3:
- #spiralRadiusIncrement = spiralRadiusIncrement * 0.7
- #if spiralRadiusIncrement < 4:
-  #spiralRadiusIncrement = spiralRadiusIncrement * 0.999
- #elif spiralRadiusIncrement < 2:
-  #spiralRadiusIncrement = spiralRadiusIncrement * 0.9
- #if spiralRadiusIncrement <= 0.25:
-  #spiralRadiusIncrement = spiralRadiusIncrement * 0.95
- #if spiralRadiusIncrement < 1:
-  #spiralRadiusIncrement = spiralRadiusIncrement * 0.99
- #if spiralRadiusIncrement <= 0.15:
-  #spiralRadiusIncrement = 0.15
+ spiralRadiusIncrement = spiralRadiusIncrement * 0.96
+ if spiralRadiusIncrement <= 0.05:
+  spiralRadiusIncrement = 0.05
 
- circleRadius = 2.1 - (i * 0.07)
- if circleRadius < 0:
+ circleRadius = 1.5 - (i * 0.05)
+ if circleRadius <= 0.1:
   circleRadius = 0.1 
-#  circleRadius = 0.5
  rowS += tagString.format(circleRadius, y, x)
  i += 1
   
@@ -54,5 +40,5 @@ f.write('<circle r="30" cx="30" cy="30" fill="none" stroke="black" stroke-width=
 )
 #f.write('<circle r="1.8" cx="30" cy="30" fill="none" stroke="black" stroke-width="0.01mm" />')
 f.write(rowS)
-f.write('<text x="31" y="57" font-size="2.5">vericite</text>')
+f.write('<text x="26" y="57" font-size="2.5">vericite</text>')
 f.write('</svg>')
