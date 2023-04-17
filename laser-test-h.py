801feceb18a0ac8c7e57b70a97e7889581df4d78
joseherminiocollas <joseherminiocollas@goatstone.com>
2023-04-16 21:49:35 -0400

    WIP

--- laser-test-h.py
+++ laser-test-h.py
@@ -5,41 +5,42 @@ offSet = 30
 #r = 0 # math.pi * 0.6 # spiral radius starr
 steps = 100
 currAngle = 0
-angleIncrement = math.pi * 0.3 # * 0.06
-spiralRadius = 0
-spiralRadiusIncrement = 5
+angleIncrement = math.pi * 0.25 # * 0.06
+spiralRadius = 3
+spiralRadiusIncrement = 0.75
 tagString = '<circle r="{0}" cx="{2}" cy="{1}" fill="none" stroke="black" stroke-width="0.01mm" />'
 
 i = 0
 while i <= steps:
- #angleIncrement = angleIncrement * 0.9
  angleX = currAngle
  angleY = currAngle
- #angleIncrement = 0.5
- #angleIncrement = angleIncrement - 0.05
- #angleIncrement = math.pi
  currAngle = currAngle + angleIncrement
- angleIncrement = angleIncrement * 0.8
- if angleIncrement < 0.3:
-  angleIncrement = 0.3
- #spiralRadius = spiralRadiusOffset
- # as spiral moves outward decrease the size that the spiral is increased
- # quantity to add each iteration
- #a = 5 - i * 2
- #if a < 0.2:
-  #a = 0.2
- #spiralRadiusOffset = spiralRadius + a # - (i * 0.1)
- #spiralRadius = 10
+ #angleIncrement = angleIncrement * 0.95
+ #if angleIncrement < 0.6:
+  #angleIncrement = angleIncrement * 0.95
+ #if angleIncrement <= 0.15:
+  #angleIncrement = 0.15
+
  x = math.cos(angleX) * spiralRadius + offSet
  y = math.sin(angleY) * spiralRadius + offSet
  spiralRadius = spiralRadius + spiralRadiusIncrement
- spiralRadiusIncrement = spiralRadiusIncrement * 0.55
- if spiralRadiusIncrement < 0.2:
-  spiralRadiusIncrement = 0.2
- circleRadius = 2 - (i * 0.05)
+ #if i < 3:
+ #spiralRadiusIncrement = spiralRadiusIncrement * 0.7
+ #if spiralRadiusIncrement < 4:
+  #spiralRadiusIncrement = spiralRadiusIncrement * 0.999
+ #elif spiralRadiusIncrement < 2:
+  #spiralRadiusIncrement = spiralRadiusIncrement * 0.9
+ #if spiralRadiusIncrement <= 0.25:
+  #spiralRadiusIncrement = spiralRadiusIncrement * 0.95
+ #if spiralRadiusIncrement < 1:
+  #spiralRadiusIncrement = spiralRadiusIncrement * 0.99
+ #if spiralRadiusIncrement <= 0.15:
+  #spiralRadiusIncrement = 0.15
+
+ circleRadius = 2.1 - (i * 0.07)
  if circleRadius < 0:
   circleRadius = 0.1 
- #circleRadius = 0.3
+#  circleRadius = 0.5
  rowS += tagString.format(circleRadius, y, x)
  i += 1
   
