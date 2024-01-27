39e9da74c62dc3f5426419c8082d4c339c5b80ee
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2024-01-27 17:58:28 -0500

    update with gray scale image

--- mona-small.py
+++ mona-small.py
@@ -3,13 +3,13 @@
 # that will be used for laser cutting
 import cv2
 
-img = cv2.imread('mona-small.png',0)
+img = cv2.imread('mona-small-gray-scale.png',0)
 fileName = 'mona-small-1-26.svg'
 r,c = img.shape
 rows = ''
 # min, max sizes in mm for laser cutter instruction
 min = 0
-max = 1
+max = 1.5
 # advance the drawing of the
 grid_cell_size = 2
 for i in range(r):
@@ -19,9 +19,9 @@ for i in range(r):
   p = k / 255
   cx = grid_cell_size * j
   cy = grid_cell_size * i
-  diaphram_size = round(p * max, 1)
-  # print(k, p, diaphram_size )
-  rows += '<circle r="{}mm" cx="{}mm" cy="{}mm" />'.format( diaphram_size, cx, cy)
+  diaphram_size_radius = round((p * max)/2, 1)
+  print(k, p, diaphram_size_radius )
+  rows += '<circle r="{}mm" cx="{}mm" cy="{}mm" />'.format( diaphram_size_radius, cx, cy)
 
 svg_file_contents =  '<svg width="200mm" height="300mm">'
 svg_file_contents += rows
