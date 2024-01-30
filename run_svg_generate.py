fc7226272c836bb8236559aa17a9f4b8da8ffb97
Jose Collas <joseherminiocollas@goatstone.com>
2024-01-30 07:49:21 -0500

    process image,  update run script, generate image according to README

--- run_svg_generate.py
+++ run_svg_generate.py
@@ -1,11 +1,11 @@
 from svg_generate import svg_generate
 
-read_image_path = 'source_images/mona-small-gray-scale.png'
-svg_file_name = 'generated_files/mona-small-2024-1-28.svg'
+read_image_path = 'source_images/mona-small-gray-2024-1-30.png'
+svg_file_name = 'generated_files/mona-small-2024-1-30.svg'
 max_diaphragm_size = 1.5
 grid_cell_size = 2
-width = 200
-height = 300
+width = 100
+height = 150
 svg_file_contents = svg_generate(
     read_image_path, max_diaphragm_size, grid_cell_size, width, height
 )
